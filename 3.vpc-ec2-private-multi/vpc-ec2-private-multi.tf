d9295da34d4c0aa832f8260aab9c768217bf4f02
DESKTOP-MIIFI3M\kdi <skypentum@gmail.com>
2025-03-03 12:47:32 +0900

    nat 게이트웨이 추가를 통한 private ec2 인터넷 nat 처리리

--- 3.vpc-ec2-private-multi/vpc-ec2-private-multi.tf
+++ 3.vpc-ec2-private-multi/vpc-ec2-private-multi.tf
@@ -177,30 +177,30 @@ resource "aws_instance" "vpc-ec2-private-os-ubuntu" {
 #   }
 # }
 
-# resource "aws_eip" "vpc-ec2-private-nat-eip" {
-#   domain = "vpc"
-# }
+resource "aws_eip" "vpc-ec2-private-nat-eip" {
+  domain = "vpc"
+}
 
-# resource "aws_nat_gateway" "vpc-ec2-private-nat" {
-#   allocation_id = aws_eip.vpc-ec2-private-nat-eip.id
-#   subnet_id     = aws_subnet.vpc-ec2-private-subnet.id
-#   tags = {
-#     Name = "nat-gateway"
-#   }
-# }
+resource "aws_nat_gateway" "vpc-ec2-private-nat" {
+  allocation_id = aws_eip.vpc-ec2-private-nat-eip.id
+  subnet_id     = aws_subnet.vpc-ec2-public-subnet.id
+  tags = {
+    Name = "nat-gateway"
+  }
+}
 
-# resource "aws_route_table" "vpc-ec2-private-rt" {
-#   vpc_id = aws_vpc.vpc-ec2-private.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.vpc-ec2-private-nat.id
-#   }
-#   tags = {
-#     Name = "vpc-ec2-private-route-table"
-#   }
-# }
+resource "aws_route_table" "vpc-ec2-private-rt" {
+  vpc_id = aws_vpc.vpc-ec2-private.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.vpc-ec2-private-nat.id
+  }
+  tags = {
+    Name = "vpc-ec2-private-route-table"
+  }
+}
 
-# resource "aws_route_table_association" "vpc-ec2-private-rt-association" {
-#   subnet_id      = aws_subnet.vpc-ec2-private-subnet.id
-#   route_table_id = aws_route_table.vpc-ec2-private-rt.id
-# }
+resource "aws_route_table_association" "vpc-ec2-private-rt-association" {
+  subnet_id      = aws_subnet.vpc-ec2-private-subnet.id
+  route_table_id = aws_route_table.vpc-ec2-private-rt.id
+}
