aa8ea7bb9f4a90a97a1508c19cb7f769edb2ca4f
DESKTOP-MIIFI3M\kdi <skypentum@gmail.com>
2025-03-03 11:58:34 +0900

    단일 ec2 public subnet에 생성 및 ssh 접속속

--- 2.vpc-ec2-public/variables.tf
+++ 2.vpc-ec2-public/variables.tf
@@ -13,3 +13,9 @@ variable "aws_use_region" {
   type        = string
   default = "ap-northeast-2"
 }
+
+variable "key_pair_name" {
+  description = "기존에 생성된 AWS Key Pair 이름"
+  type        = string
+  default = "test-common-key"
+}
--- 2.vpc-ec2-public/vpc-ec2-public.tf
+++ 2.vpc-ec2-public/vpc-ec2-public.tf
@@ -3,15 +3,13 @@ provider "aws" {
   region = var.aws_use_region
 }
 
-# 아래 명령어를 사용하여 ssh 공개 키 생성
-# ssh-keygen -t rsa -b 2048 -f ssh-key/id_rsa 
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "default-key-pair"
-  public_key = file("../ssh-key/id_rsa.pub") # 로컬 공개키 경로 설정
+data "aws_key_pair" "existing_key" {
+  key_name           = var.key_pair_name
+  include_public_key = true # 공개 키도 조회 (선택 사항)
 }
 
 resource "aws_vpc" "vpc-ec2-public" {
-  cidr_block = "10.1.30.0/24"
+  cidr_block = "10.10.30.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -19,15 +17,35 @@ resource "aws_vpc" "vpc-ec2-public" {
   }
 }
 
+resource "aws_internet_gateway" "vpc-public-igw" {
+  vpc_id = aws_vpc.vpc-ec2-public.id
+  tags = {
+    Name = "vpc-public-igw"
+  }
+}
+
+# Public Subnet Route Table (Public 서브넷은 IGW를 통해 인터넷 연결)
+resource "aws_route_table" "vpc-public-rt" {
+  vpc_id = aws_vpc.vpc-ec2-public.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc-public-igw.id # ✅ IGW를 통해 인터넷 연결
+  }
+  tags = {
+    Name = "vpc-public-route-table"
+  }
+}
+
 resource "aws_subnet" "vpc-ec2-public-subnet" {
   vpc_id            = aws_vpc.vpc-ec2-public.id
-  cidr_block        = "10.1.30.0/24"
+  cidr_block        = "10.10.30.0/24"
   availability_zone = "ap-northeast-2a" # ec2 생성 시, 해당 대역대와 동일한 AZ 구성 필요
   map_public_ip_on_launch = true
   tags = {
     Name = "vpc-ec2-public-subnet"
   }
 }
+
 
 resource "aws_security_group" "vpc-ec2-public-ec2-sg" {
   vpc_id = aws_vpc.vpc-ec2-public.id
@@ -36,7 +54,7 @@ resource "aws_security_group" "vpc-ec2-public-ec2-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["211.60.209.194/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -51,11 +69,10 @@ resource "aws_security_group" "vpc-ec2-public-ec2-sg" {
   }
 }
 
-resource "aws_internet_gateway" "vpc-ec2-public-igw" {
-  vpc_id = aws_vpc.vpc-ec2-public.id
-  tags = {
-    Name = "vpc-ec2-public-igw"
-  }
+# 서브넷과 라우팅 테이블 연결 (서브넷 1)
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.vpc-ec2-public-subnet.id
+  route_table_id = aws_route_table.vpc-public-rt.id
 }
 
 resource "aws_instance" "vpc-ec2-public-os-amazon-linux" {
@@ -63,7 +80,7 @@ resource "aws_instance" "vpc-ec2-public-os-amazon-linux" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.vpc-ec2-public-subnet.id
   availability_zone = "ap-northeast-2a" # 변경 가능
-  key_name      = aws_key_pair.ssh-key.key_name
+  key_name      = data.aws_key_pair.existing_key.key_name
   security_groups = [aws_security_group.vpc-ec2-public-ec2-sg.id]
   tags = {
     Name = "vpc-ec2-public-os-amazon-linux-Instance"
