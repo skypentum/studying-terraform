8552acc16065c7b92edbc44d323746f6002a3840
DESKTOP-MIIFI3M\kdi <skypentum@gmail.com>
2025-03-03 12:26:23 +0900

    private ec2 설정 변경경

--- 3.vpc-ec2-private-multi/vpc-ec2-private-multi.tf
+++ 3.vpc-ec2-private-multi/vpc-ec2-private-multi.tf
@@ -4,15 +4,13 @@ provider "aws" {
   region = var.aws_use_region
 }
 
-# 아래 명령어를 사용하여 ssh 공개 키 생성
-# ssh-keygen -t rsa -b 2048 -f ssh-key/id_rsa 
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "default-key-pair"
-  public_key = file("../ssh-key/id_rsa.pub") # 로컬 공개키 경로 설정
+data "aws_key_pair" "existing_key" {
+  key_name           = var.key_pair_name
+  include_public_key = true # 공개 키도 조회 (선택 사항)
 }
 
 resource "aws_vpc" "vpc-ec2-private" {
-  cidr_block = "10.1.30.0/24"
+  cidr_block = "10.20.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -20,9 +18,78 @@ resource "aws_vpc" "vpc-ec2-private" {
   }
 }
 
+resource "aws_internet_gateway" "vpc-public-igw" {
+  vpc_id = aws_vpc.vpc-ec2-private.id
+  tags = {
+    Name = "vpc-public-igw"
+  }
+}
+
+# Public Subnet Route Table (Public 서브넷은 IGW를 통해 인터넷 연결)
+resource "aws_route_table" "vpc-public-rt" {
+  vpc_id = aws_vpc.vpc-ec2-private.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc-public-igw.id # ✅ IGW를 통해 인터넷 연결
+  }
+  tags = {
+    Name = "vpc-public-route-table"
+  }
+}
+
+resource "aws_subnet" "vpc-ec2-public-subnet" {
+  vpc_id            = aws_vpc.vpc-ec2-private.id
+  cidr_block        = "10.20.30.0/24"
+  availability_zone = "ap-northeast-2a" # ec2 생성 시, 해당 대역대와 동일한 AZ 구성 필요
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "vpc-ec2-public-subnet"
+  }
+}
+
+resource "aws_security_group" "vpc-ec2-public-ec2-sg" {
+  vpc_id = aws_vpc.vpc-ec2-private.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-ec2-public-ec2-security-group"
+  }
+}
+
+# 서브넷과 라우팅 테이블 연결 (서브넷 1)
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.vpc-ec2-public-subnet.id
+  route_table_id = aws_route_table.vpc-public-rt.id
+}
+
+resource "aws_instance" "vpc-ec2-public-os-amazon-linux" {
+  ami           = "ami-0a998385ed9f45655" # Amazon Linux AMI ID (defult)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.vpc-ec2-public-subnet.id
+  availability_zone = "ap-northeast-2a" # 변경 가능
+  key_name      = data.aws_key_pair.existing_key.key_name
+  security_groups = [aws_security_group.vpc-ec2-public-ec2-sg.id]
+  tags = {
+    Name = "vpc-ec2-public-os-amazon-linux-Instance"
+  }
+}
+
 resource "aws_subnet" "vpc-ec2-private-subnet" {
   vpc_id            = aws_vpc.vpc-ec2-private.id
-  cidr_block        = "10.1.30.0/24"
+  cidr_block        = "10.20.130.0/24"
   availability_zone = "ap-northeast-2a" # ec2 생성 시, 해당 대역대와 동일한 AZ 구성 필요
   map_public_ip_on_launch = false
   tags = {
@@ -36,13 +103,6 @@ resource "aws_security_group" "vpc-ec2-private-ec2-sg" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["211.60.209.194/32"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -59,16 +119,9 @@ resource "aws_security_group" "vpc-ec2-private-ec2-sg" {
   }
 }
 
-resource "aws_internet_gateway" "vpc-ec2-private-igw" {
-  vpc_id = aws_vpc.vpc-ec2-private.id
-  tags = {
-    Name = "vpc-ec2-private-igw"
-  }
-}
-
 resource "aws_network_interface" "ec2-private-os-amazon-linux" {
   subnet_id = aws_subnet.vpc-ec2-private-subnet.id
-  private_ips = ["10.1.30.10"]
+  private_ips = ["10.20.130.10"]
   security_groups = [aws_security_group.vpc-ec2-private-ec2-sg.id]
   tags = {
     Name = "ec2-private-os-amazon-linux-private-network"
@@ -79,7 +132,7 @@ resource "aws_instance" "vpc-ec2-private-os-amazon-linux" {
   ami           = "ami-0a998385ed9f45655" # Amazon Linux AMI ID (defult)
   instance_type = "t2.micro"
   availability_zone = "ap-northeast-2a" # 변경 가능
-  key_name      = aws_key_pair.ssh-key.key_name  
+  key_name      = data.aws_key_pair.existing_key.key_name
   network_interface {
     network_interface_id = aws_network_interface.ec2-private-os-amazon-linux.id
     device_index         = 0
@@ -91,7 +144,7 @@ resource "aws_instance" "vpc-ec2-private-os-amazon-linux" {
 
 resource "aws_network_interface" "ec2-private-os-ubuntu" {
   subnet_id = aws_subnet.vpc-ec2-private-subnet.id
-  private_ips = ["10.1.30.11"]
+  private_ips = ["10.20.130.11"]
   security_groups = [aws_security_group.vpc-ec2-private-ec2-sg.id]
   tags = {
     Name = "ec2-private-os-ubuntu-private-network"
@@ -102,7 +155,7 @@ resource "aws_instance" "vpc-ec2-private-os-ubuntu" {
   ami           = "ami-024ea438ab0376a47" # Ubuntu AMI ID (defult)
   instance_type = "t2.micro"
   availability_zone = "ap-northeast-2a" # 변경 가능
-  key_name      = aws_key_pair.ssh-key.key_name  
+  key_name      = data.aws_key_pair.existing_key.key_name
   network_interface {
     network_interface_id = aws_network_interface.ec2-private-os-ubuntu.id
     device_index         = 0
@@ -124,30 +177,30 @@ resource "aws_instance" "vpc-ec2-private-os-ubuntu" {
 #   }
 # }
 
-resource "aws_eip" "vpc-ec2-private-nat-eip" {
-  domain = "vpc"
-}
+# resource "aws_eip" "vpc-ec2-private-nat-eip" {
+#   domain = "vpc"
+# }
 
-resource "aws_nat_gateway" "vpc-ec2-private-nat" {
-  allocation_id = aws_eip.vpc-ec2-private-nat-eip.id
-  subnet_id     = aws_subnet.vpc-ec2-private-subnet.id
-  tags = {
-    Name = "nat-gateway"
-  }
-}
+# resource "aws_nat_gateway" "vpc-ec2-private-nat" {
+#   allocation_id = aws_eip.vpc-ec2-private-nat-eip.id
+#   subnet_id     = aws_subnet.vpc-ec2-private-subnet.id
+#   tags = {
+#     Name = "nat-gateway"
+#   }
+# }
 
-resource "aws_route_table" "vpc-ec2-private-rt" {
-  vpc_id = aws_vpc.vpc-ec2-private.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.vpc-ec2-private-nat.id
-  }
-  tags = {
-    Name = "vpc-ec2-private-route-table"
-  }
-}
+# resource "aws_route_table" "vpc-ec2-private-rt" {
+#   vpc_id = aws_vpc.vpc-ec2-private.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.vpc-ec2-private-nat.id
+#   }
+#   tags = {
+#     Name = "vpc-ec2-private-route-table"
+#   }
+# }
 
-resource "aws_route_table_association" "vpc-ec2-private-rt-association" {
-  subnet_id      = aws_subnet.vpc-ec2-private-subnet.id
-  route_table_id = aws_route_table.vpc-ec2-private-rt.id
-}
+# resource "aws_route_table_association" "vpc-ec2-private-rt-association" {
+#   subnet_id      = aws_subnet.vpc-ec2-private-subnet.id
+#   route_table_id = aws_route_table.vpc-ec2-private-rt.id
+# }
